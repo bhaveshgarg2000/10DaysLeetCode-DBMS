f218560ab4096ed4cba17e70a3ed6063031d5f2c
bhaveshgarg2000 <bhaveshgarg2005@gmail.com>
2023-01-06 15:12:10 +0530

    Day 4 Done

---
+++ 1965employeewithMissingInfo.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+Select employee_id from employees where employee_id not in (select employee_id from salaries)
+union
+select employee_id from salaries where employee_id not in(select employee_id from employees) order by employee_id asc;
